97ecb992fbfa7ebecfc6059780d030965a876dc3
Boz-Yusuf <boz35yusuf@gmail.com>
2023-08-25 23:41:49 +0300

    Add project files.

---
+++ PrimeNumber.Core/Entities/CalculatedSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber.Core.Entities
+{
+    public class CalculatedSet
+    {
+        public int Id { get; set; }
+        public int BiggestPrimeNumber { get; set; }
+        public List<int> NumberSet { get; set; }
+    }
+}
