b6bf65b1342994a681decd4e1404a4bc131ebb16
Boz-Yusuf <boz35yusuf@gmail.com>
2023-08-26 00:03:29 +0300

    Core Layer was implemented

---
+++ PrimeNumber.Core/Repositories/IPrimeNumberRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrimeNumber.Core.Entities;
+
+namespace PrimeNumber.Core.Repositories
+{
+    public interface IPrimeNumberRepository
+    {
+        Task<CalculatedSet> GetByIdAsync(int id);
+        IEnumerable<Task<CalculatedSet>> GetAllAsync();
+        Task AddAsync(CalculatedSet set);
+        void Update(CalculatedSet set);
+        void Remove(CalculatedSet set);
+    }
+}
---
+++ PrimeNumber.Core/Service/IPrimeNumberService.cs
@@ -0,0 +1,18 @@
+﻿using PrimeNumber.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber.Core.Service
+{
+    public interface IPrimeNumberService
+    {
+        Task<CalculatedSet> GetByIdAsync(int id);
+        Task<IEnumerable<CalculatedSet>> GetAllAsync();
+        Task<CalculatedSet> AddAsync(CalculatedSet set);
+        Task UpdateAsync(CalculatedSet set);
+        Task RemoveAsync(CalculatedSet set);
+    }
+}
